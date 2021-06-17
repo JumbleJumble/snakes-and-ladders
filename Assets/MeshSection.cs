883c2d3359a214e06a6d35bb49543dfabe0dc7ca
Richard Irons <richard.irons@five.ai>
2021-06-17 14:09:35 +0100

    tail generation now not a sharpened pencil

---
+++ Assets/MeshSection.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SnakesAndLadders
+{
+    public class MeshSection
+    {
+        private readonly List<Vector3> vertices = new List<Vector3>();
+        private readonly List<Vector3> normals = new List<Vector3>();
+        private readonly List<int> triangles = new List<int>();
+
+        /// <summary>
+        /// Adds a vertex to the section with its normal.
+        /// </summary>
+        /// <returns>The index of the just-added vertex.</returns>
+        public int AddVertex(Vector3 vertex, Vector3 normal)
+        {
+            vertices.Add(vertex);
+            normals.Add(normal);
+            return vertices.Count - 1;
+        }
+
+        public void AddTriangle(int a, int b, int c)
+        {
+            triangles.Add(a);
+            triangles.Add(b);
+            triangles.Add(c);
+        }
+
+        public int NextVertexIndex => vertices.Count;
+
+        public void Deconstruct(out Vector3[] outVertices, out Vector3[] outNormals, out int[] outTriangles)
+        {
+            outVertices = vertices.ToArray();
+            outNormals = normals.ToArray();
+            outTriangles = triangles.ToArray();
+        }
+    }
+}
--- Assets/SnakeGenerator.cs
+++ Assets/SnakeGenerator.cs
@@ -1,102 +1,198 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Assertions;
 
-[RequireComponent(typeof(MeshRenderer))]
-[RequireComponent(typeof(MeshFilter))]
-[ExecuteInEditMode]
-public class SnakeGenerator : MonoBehaviour
+namespace SnakesAndLadders
 {
-    private MeshRenderer meshRenderer;
-    private MeshFilter meshFilter;
-    private bool dirty;
-
-    const int numSections = 16;
-    const float sectionLength = 0.1f;
-    private const float tailLength = 0.15f;
-    const int numSides = 32;
-    const float radius = 0.1f;
-
-    private void Awake()
+    [RequireComponent(typeof(MeshRenderer))]
+    [RequireComponent(typeof(MeshFilter))]
+    [ExecuteInEditMode]
+    public class SnakeGenerator : MonoBehaviour
     {
-        meshRenderer = GetComponent<MeshRenderer>();
-        meshFilter = GetComponent<MeshFilter>();
-    }
+        private MeshFilter meshFilter;
+        private bool dirty;
 
-    private void OnEnable()
-    {
-        dirty = true;
-    }
+        public int numSections = 16;
+        public float sectionLength = 0.1f;
 
-    private void GenerateSnake()
-    {
-        var mesh = new Mesh() { name = "snake mesh" };
-        var vertices = new List<Vector3>();
-        var normals = new List<Vector3>();
-        var triangles = new List<int>();
-        var tx = transform;
-        vertices.Add(tx.position);
-        normals.Add(-tx.up);
-        for (int section = 0; section <= numSections; section++)
+        [Range(3, 48)]
+        public int numSides = 3;
+
+        [Min(0)]
+        public float bodyRadius = 0.5f;
+
+        [Range(1, 36)]
+        public int tailSections = 10;
+
+        [Min(0)]
+        public float tailLength = 0.2f;
+
+        [Range(0, 5f)]
+        public float tailCurvePower = 0.8f;
+
+        [Min(1)]
+        public float tailCurveBias = 2;
+
+        private void Awake()
         {
-            var centre = tx.position + tx.up * (sectionLength * section + tailLength);
-            if (section > 0)
-            {
-                for (int side = 0; side < numSides; side++)
-                {
-                    var q = Quaternion.AngleAxis(side * 360f / numSides, tx.up);
-                    var vertex = centre + q * tx.forward * radius;
-                    vertices.Add(vertex);
-                    var normal = -(centre - vertex).normalized / 3f;
-                    normals.Add(normal);
-
-                    int sideBase = (section - 1) * numSides + 1;
-                    int vertexIdx = vertices.Count - 1;
-                    if (section == 1)
-                    {
-                        AddTriangle(triangles, vertexIdx, 0, vertexIdx % numSides + 1);
-                    }
-                    else
-                    {
-                        int lowerSideBase = (section - 2) * numSides + 1;
-                        var quad = new[]
-                        {
-                            sideBase + side, sideBase + (side + 1) % numSides,
-                            lowerSideBase + (side + 1) % numSides, lowerSideBase + side
-                        };
-
-                        AddTriangle(triangles, quad[0], quad[2], quad[1]);
-                        AddTriangle(triangles, quad[0], quad[3], quad[2]);
-                    }
-                }
-            }
+            meshFilter = GetComponent<MeshFilter>();
         }
 
-        Assert.AreEqual(vertices.Count, normals.Count);
-        mesh.vertices = vertices.ToArray();
-        mesh.normals = normals.ToArray();
-        mesh.triangles = triangles.ToArray();
-        meshFilter.mesh = mesh;
-    }
-
-    private static void AddTriangle(List<int> triangles, int point1, int point2, int point3)
-    {
-        triangles.Add(point1);
-        triangles.Add(point2);
-        triangles.Add(point3);
-    }
-
-    // Start is called before the first frame update
-    private void Start()
-    {
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
-        if (dirty)
+        private void OnEnable()
         {
-            GenerateSnake();
+            dirty = true;
+        }
+
+        private void GenerateSnake()
+        {
+            var (tail, _) = CreateTail(Vector3.zero);
+            var (vtc, nrm, tri) = tail;
+            var msh = new Mesh { name = "snake mesh", vertices = vtc, normals = nrm, triangles = tri };
+            meshFilter.mesh = msh;
+            return;
+
+            //
+            // var mesh = new Mesh() { name = "snake mesh" };
+            // var vertices = new List<Vector3>();
+            // var normals = new List<Vector3>();
+            // var triangles = new List<int>();
+            // vertices.Add(Vector3.zero);
+            // normals.Add(Vector3.back);
+            // for (int section = 0; section <= numSections; section++)
+            // {
+            //     var centre = Vector3.forward * (sectionLength * section + tailLength);
+            //     if (section > 0)
+            //     {
+            //         for (int side = 0; side < numSides; side++)
+            //         {
+            //             var q = Quaternion.AngleAxis(side * 360f / numSides, Vector3.forward);
+            //             var vertex = centre + q * Vector3.up * bodyRadius;
+            //             vertices.Add(vertex);
+            //             var normal = -(centre - vertex).normalized / 3f;
+            //             normals.Add(normal);
+            //
+            //             int sideBase = (section - 1) * numSides + 1;
+            //             int vertexIdx = vertices.Count - 1;
+            //             if (section == 1)
+            //             {
+            //                 AddTriangle(triangles, vertexIdx, 0, vertexIdx % numSides + 1);
+            //             }
+            //             else
+            //             {
+            //                 int lowerSideBase = (section - 2) * numSides + 1;
+            //                 var quad = new[]
+            //                 {
+            //                     sideBase + side, sideBase + (side + 1) % numSides,
+            //                     lowerSideBase + (side + 1) % numSides, lowerSideBase + side
+            //                 };
+            //
+            //                 AddTriangle(triangles, quad[0], quad[2], quad[1]);
+            //                 AddTriangle(triangles, quad[0], quad[3], quad[2]);
+            //             }
+            //         }
+            //     }
+            // }
+            //
+            // Assert.AreEqual(vertices.Count, normals.Count);
+            // mesh.vertices = vertices.ToArray();
+            // mesh.normals = normals.ToArray();
+            // mesh.triangles = triangles.ToArray();
+            // meshFilter.mesh = mesh;
+        }
+
+        private static void AddTriangle(List<int> triangles, int point1, int point2, int point3)
+        {
+            triangles.Add(point1);
+            triangles.Add(point2);
+            triangles.Add(point3);
+        }
+
+        // SQRT(1-((1-A2)^$E$1)^2)
+        private float TailCurveFunc(float tailPos) =>
+            Mathf.Sqrt(1 - Mathf.Pow(Mathf.Pow(1 - tailPos, tailCurvePower), 2));
+
+        private (MeshSection tailMesh, Vector3 continuationPoint) CreateTail(Vector3 startPoint)
+        {
+            var tailCore = startPoint + Vector3.forward * tailLength;
+
+            // first point is the very center of the end of the tail
+            var tail = new MeshSection();
+            tail.AddVertex(startPoint, Vector3.back);
+
+            var capRadius = TailCurveFunc(Mathf.Pow(0.0001f / tailSections, tailCurveBias)) * bodyRadius;
+            var tailCap = GetVerticesAroundCenter(startPoint, capRadius, false);
+            for (var i = 0; i < tailCap.Length; i++)
+            {
+                tail.AddVertex(tailCap[i], tailCap[i] - tailCore);
+                tail.AddTriangle(i + 1, 0, (i + 1) % numSides + 1);
+            }
+
+            bool twist = true;
+            for (var s = 0; s < tailSections; s++)
+            {
+                float secPos = (float) (s + 1) / tailSections;
+                var currentPos = Mathf.Pow(secPos, tailCurveBias);
+
+                var v = tail.NextVertexIndex;
+                int ThisRow(int n) => v + n % numSides;
+                int PrevRow(int n) => v - numSides + (n + numSides) % numSides;
+
+                var tailPoints = GetVerticesAtPos(currentPos, twist, startPoint);
+                for (var i = 0; i < tailPoints.Length; i++)
+                {
+                    var adjacentVertex = tailPoints[(i - 1 + numSides) % numSides];
+                    var normal = Vector3.Cross(tailPoints[i], adjacentVertex);
+                    tail.AddVertex(tailPoints[i], normal);
+
+                    var a = ThisRow(i);
+                    var b = ThisRow(i + 1);
+                    var t = PrevRow(i - (twist ? 0 : 1));
+                    var u = PrevRow(i + (twist ? 1 : 0));
+                    tail.AddTriangle(a, t, u);
+                    tail.AddTriangle(a, u, b);
+                }
+
+                twist = !twist;
+            }
+
+            return (tail, tailCore);
+        }
+
+        private Vector3[] GetVerticesAtPos(float sectionPos, bool twist, Vector3 startPoint)
+        {
+            var posRadius = TailCurveFunc(sectionPos) * bodyRadius;
+            var center = startPoint + sectionPos * tailLength * Vector3.forward;
+            Vector3[] result = GetVerticesAroundCenter(center, posRadius, twist);
+            return result;
+        }
+
+        private Vector3[] GetVerticesAroundCenter(Vector3 center, float radius, bool twist)
+        {
+            var first = Vector3.up * radius;
+            var segmentAngle = 360f / numSides;
+            var angleOffset = twist ? segmentAngle / 2f : 0;
+            var result = new Vector3[numSides];
+            for (int i = 0; i < numSides; i++)
+            {
+                var angle = i * segmentAngle + angleOffset;
+                var rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+                result[i] = center + rotation * first;
+            }
+
+            return result;
+        }
+
+        private void OnValidate()
+        {
+            dirty = true;
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            if (dirty)
+            {
+                GenerateSnake();
+            }
         }
     }
 }
